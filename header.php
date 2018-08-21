5638371f5cfd88a9fbaca3b675b97050d8dec28a
Roy Jossfolk <rjossfolk@gmail.com>
2018-08-21 18:00:26 -0400

    add double arrow after sub-menu items with children

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700|Open+Sans:300,400,600,700,800" rel="stylesheet"></link>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700|Open+Sans:100,200,300,400,600,700,800" rel="stylesheet"></link>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"></link>
   
 	

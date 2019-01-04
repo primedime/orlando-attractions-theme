f01c1ae3bea6b314476c9b78f9ff59d2e18777ab
Roy Jossfolk <rjossfolk@gmail.com>
2019-01-04 15:18:34 -0500

    exclude additional category from home page article hero

--- template-parts/home/article-hero.php
+++ template-parts/home/article-hero.php
@@ -31,7 +31,7 @@
         $next_recent_posts = wp_get_recent_posts(array(
           'numberposts' => 3, // Number of recent posts thumbnails to display
           'offset' => 1,
-          'category__not_in' => array(447, 450, 457, 475), // exclude specific partner categories
+          'category__not_in' => array(435, 447, 450, 457, 475), // exclude specific partner categories
           'post_status' => 'publish' // Show only the published posts
         ));
       foreach($next_recent_posts as $post) : ?>

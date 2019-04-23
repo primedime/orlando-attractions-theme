17e8d6d1d1fc1ebd58aa26895d5b661b9841c7b7
Roy Jossfolk <rjossfolk@gmail.com>
2019-04-23 14:34:41 -0400

    fix search results page

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,15 +11,15 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			orlando_attractions_posted_on();
-			orlando_attractions_posted_by();
-			?>
-		</div><!-- .entry-meta -->
+		<?php if ('post' === get_post_type()) : ?>
+			<div class="entry-meta">
+				<?php
+				orlando_attractions_posted_on();
+				orlando_attractions_posted_by();
+				?>
+			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
@@ -29,7 +29,5 @@
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php orlando_attractions_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+
 </article><!-- #post-<?php the_ID(); ?> -->
